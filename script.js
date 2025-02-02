c0b35ca40e96dfa27335f3d37fb5aaaa779c5cc9
Mikael Ferreira <142128917+mikaelfmts@users.noreply.github.com>
2025-02-02 04:40:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    let menu = document.getElementById("menu-lateral");
+    
+    if (menu.classList.contains("menu-aberto")) {
+        menu.classList.remove("menu-aberto");
+    } else {
+        menu.classList.add("menu-aberto");
+    }
+}

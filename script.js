05d029c3aabb50f4accb0b5e43bf6686eef5ccb8
Mikael Ferreira <142128917+mikaelfmts@users.noreply.github.com>
2025-02-02 21:57:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function toggleMenu() {
         menu.classList.add("menu-aberto");
     }
 }
+
+// Função para verificar quando os projetos entram na tela
+document.addEventListener("DOMContentLoaded", function () {
+    const projetos = document.querySelector("#projetos");
+    function verificarVisibilidade() {
+        const posicao = projetos.getBoundingClientRect().top;
+        if (posicao < window.innerHeight - 100) {
+            projetos.classList.add("projetos-visiveis");
+        }
+    }
+    window.addEventListener("scroll", verificarVisibilidade);
+    verificarVisibilidade();
+});

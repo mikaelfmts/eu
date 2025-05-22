40536a772fa011d94f52fc0a08b89094ae463360
Mikael Ferreira <142128917+mikaelfmts@users.noreply.github.com>
2025-05-22 14:32:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+// Service Worker para PWA
+const CACHE_NAME = 'portfolio-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
 

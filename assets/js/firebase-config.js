788d92f2db61321bffeef1ac4b2a8f23e9957067
Mikael Ferreira <142128917+mikaelfmts@users.noreply.github.com>
2025-05-23 13:31:23 -0300

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+// Configuração do Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOxxBvg9eeRJLAgB3vW51PvM9hD9mmkW4",
+  authDomain: "mikaelfmts.firebaseapp.com",
+  projectId: "mikaelfmts",
+  storageBucket: "mikaelfmts.appspot.com",
+  messagingSenderId: "947411827517",
+  appId: "1:947411827517:web:e77181d5723f7da45464cb",
+  measurementId: "G-VMCRQX81ZR"
+};
+
+// Exporta a configuração para ser usada em outros arquivos
+export default firebaseConfig;
